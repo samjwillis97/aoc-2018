532d3d94130480a85a446dde8359faf13011fd5d
Sam Willis <sam@williscloud.org>
2023-11-02 23:01:37 +1100

    Made template script on index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello, World!")
+import { copyFile, copyFileSync, existsSync, mkdirSync, openSync } from "fs";
+
+if (!process.argv[2]) {
+  console.log("No argument given.");
+  process.exit(1);
+}
+
+const day = parseInt(process.argv[2]);
+console.log(`Creating directories for Day ${day}`);
+
+const workingDir = process.cwd();
+const dayDirectory = `${workingDir}/src/day${day}`;
+console.log(`Creating ${dayDirectory}`);
+
+if (existsSync(dayDirectory)) {
+  console.log("Directory already exists");
+  process.exit(1);
+}
+
+mkdirSync(dayDirectory);
+openSync(`${dayDirectory}/day${day}.ts`, 'a')
+openSync(`${dayDirectory}/day${day}.test.ts`, 'a')
+openSync(`${dayDirectory}/input.txt`, 'a')
+
+copyFileSync('src/template/day.ts', `${dayDirectory}/day${day}.ts`)
+
---
+++ src/template/day.ts
@@ -0,0 +1,5 @@
+export function part1(input: string[]) {
+}
+
+export function part2(input: string[]) {
+}
